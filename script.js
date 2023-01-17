e6a48c054c2a1f94cff7cf210b6747ef285a8af8
civilix <i@civilux.cc>
2023-01-18 02:24:07 +0900

    1

---
+++ script.js
@@ -0,0 +1,66 @@
+document.getElementById("life-expectancy").onsubmit = function() {
+    event.preventDefault(); 
+    // let gender = document.getElementById("gender").value;
+    // let dob = new Date(document.getElementById("dob").value);
+    // let smoking = document.getElementById("smoking").value;
+    // let drinking = document.getElementById("drinking").value;
+    let gender = "male";
+    let dob = new Date(2000,9,16).valueOf();
+    let smoking = "yes";
+    let drinking = "yes";
+    const timeLeft = document.getElementById("timeLeft");
+    // 予想寿命を計算する 
+    const lifeExpectancy = calculate(gender, smoking, drinking);
+    // カウントダウンをする
+    timeLeft.style.color = "red";
+    countdown(lifeExpectancy, dob);
+}
+// 予想寿命を計算する関数
+function calculate(gender, smoking, drinking) {
+    // 厚生労働省　令和３年簡易生命表の概況　主な年齢の平均余命 
+    // https://www.mhlw.go.jp/toukei/saikin/hw/life/life21/dl/life18-02.pdf
+    let result = 0;
+    if (gender == "male"){
+  	    result = 81.47;
+    }
+    else if (gender == "female"){
+  	    result = 87.57;
+    }
+    // Center for Disease Control and Prevention (CDC)
+    //https://www.cdc.gov/tobacco/data_statistics/fact_sheets/health_effects/tobacco_related_mortality/index.htm#:~:text=Cigarette%20smoking%20causes%20premature%20death,years%20shorter%20than%20for%20nonsmokers.&text=Quitting%20smoking%20before%20the%20age,related%20disease%20by%20about%2090%25.
+    if (smoking == "yes"){
+  	    result -= 10.0;
+    }
+    // Scientific Reports
+    //https://www.nature.com/articles/s41598-022-11427-x#:~:text=In%20conclusions%2C%20modest%20drinkers%2C%20no,65%E2%80%9380%25)%20of%20drinkers.
+    if (drinking == "yes"){
+        result -= 7.0;
+    }
+    return result;
+}
+// カウントダウンをする関数
+function countdown(year,dob) {
+    //「終わる日」を計算する
+    const milli = year * 365 * 24 * 60 * 60 * 1000;
+    const now = new Date().valueOf();
+    const end = dob + milli;
+    // カウントダウンをする
+    setInterval(() => {
+        const secondsLeft = Math.round((end - Date.now()) / 1000);
+        displayTimeLeft(secondsLeft);
+    },1000);
+}
+
+function displayTimeLeft(secondsLeft) {
+    console.log(secondsLeft);
+    const days = Math.floor(secondsLeft / 24 / 60 / 60);
+    secondsLeft = secondsLeft % 86400;
+    const hours = Math.floor(secondsLeft / 60 / 60);
+    secondsLeft = secondsLeft % 3600;
+    const minutes = Math.floor(secondsLeft / 60);
+    secondsLeft = secondsLeft % 60;
+    const seconds = secondsLeft;
+    //数字を2桁にする
+    const display = `${days}日${hours < 10 ? "0" : ""}${hours}時間${minutes < 10 ? "0" : ""}${minutes}分${seconds < 10 ? "0" : ""}${seconds}秒`;
+    timeLeft.textContent = display;
+}
